3f5ca5cff10e3e20a3ed58640f77819f7ef12ef7
RDXTEAM3 <krishchhasiya768@gmail.com>
2025-08-21 14:24:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,14 +6,14 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", ""))
-    API_HASH = getenv("API_HASH", "")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    API_ID = int(getenv("API_ID", "20791076"))
+    API_HASH = getenv("API_HASH", "a90db1b6cc9df12dc375fe286b2c36c4")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8475448896:AAGOeGdswAtISF2e9X5OmiwjcMX8qRKGc1M")
     # Your Force Subscribe Channel Id Below 
-    CHID = int(getenv("CHID", "")) # Make Bot Admin In This Channel
+    CHID = int(getenv("KCX BHAI", "2834246465")) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
-    SUDO = list(map(int, getenv("SUDO", "").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    SUDO = list(map(int, getenv("SUDO", "7957500694").split()))
+    MONGO_URI = getenv("MONGO_URI", "https://t.me/c/2646376491/698")
     
 cfg = Config()
 
